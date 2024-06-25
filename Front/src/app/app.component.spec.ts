2712cf753df652ed451e3c0301ffc78b92cbde38
Stelian-Constantin Stanciu <stanciu.stelian86@yahoo.fr>
2024-06-25 11:47:41 +0200

    modif

--- Front/src/app/app.component.spec.ts
+++ Front/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Cyber Web');
+    expect(compiled.querySelector('h1')?.textContent).toContain('QuizzApp');
   });
 });
