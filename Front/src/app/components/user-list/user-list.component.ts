7514db5a5c472c675181f13fe53ed0dbeeab204d
Stelian2022 <stanciu.stelian86@yahoo.fr>
2024-06-27 16:53:50 +0200

    user-list ok

--- Front/src/app/components/user-list/user-list.component.ts
+++ Front/src/app/components/user-list/user-list.component.ts
@@ -8,11 +8,10 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.css']
+  styleUrls: []
 })
 export class UserListComponent implements OnInit {
   users: User[] = [];
-
   constructor(private userService: UserService) { }
 
   ngOnInit(): void {
